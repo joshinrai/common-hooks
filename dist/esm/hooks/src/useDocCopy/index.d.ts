55c385122bd548d79562bae3db8e07b533c25b78
joshinrai <joshinrai@163.com>
2022-11-01 19:35:53 +0800

    build

--- dist/esm/hooks/src/useDocCopy/index.d.ts
+++ dist/esm/hooks/src/useDocCopy/index.d.ts
@@ -1,8 +1,5 @@
-declare const useDocCopy: (
-  message: any,
-  text?: any,
-) => {
-  setContent: import('react').Dispatch<any>;
-  setCopy: () => void;
+declare const useDocCopy: (message: any, text?: any) => {
+    setContent: import("react").Dispatch<any>;
+    setCopy: () => void;
 };
 export default useDocCopy;
--- dist/esm/index.js
+++ dist/esm/index.js
@@ -6,7 +6,7 @@
  * @FilePath: /common-hooks/src/index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import useDocCopy from './hooks/src/useDocCopy';
-import Foo from './Foo'; // export { default as Foo } from './Foo';
+import useDocCopy from "./hooks/src/useDocCopy";
+import Foo from "./Foo"; // export { default as Foo } from './Foo';
 
 export { Foo, useDocCopy };
